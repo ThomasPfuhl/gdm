54a65b5090a2a207bec8b2acb8a0fdcc541444cc
Pfuhl, Thomas <thomas.pfuhl@mfn-berlin.de>
2017-06-16 11:44:31 +0200

    dynamically added routes are created in included file more_routes.php

--- lib/tools/add_routes.php
+++ lib/tools/add_routes.php
@@ -3,19 +3,21 @@
 /*
   Route::get('dummies', 'DummiesController@index');
   Route::get('dummy/{slug}', 'DummiesController@show');
+  Route::get('dummy/data', 'ProposalsController@data');
+
  */
 
 $name = $argv[1];
-
 
 $content = <<<'PHPCODE'
 
 Route::get('NAMEs', 'CNAMEsController@index');
 Route::get('NAME/{slug}', 'CNAMEsController@show');
+Route::get('NAME/data', 'CNAMEsController@data');
 
 PHPCODE;
 
 $content = str_replace('CNAME', ucfirst($name), $content);
 $content = str_replace('NAME', $name, $content);
 
-file_put_contents("../../app/Http/routes.php", $content, FILE_APPEND | LOCK_EX);
+file_put_contents("../../app/Http/more_routes.php", $content, FILE_APPEND | LOCK_EX);
