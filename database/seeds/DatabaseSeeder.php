dc83d3af77e47f59ed2a55b11e4737cf27b56665
Glöckler, Falko <falko.gloeckler@mfn-berlin.de>
2017-06-30 15:27:25 +0200

    Initial seeds for users table (default admin user)

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -15,17 +15,11 @@ class DatabaseSeeder extends Seeder {
         Model::unguard();
 
         // Add calls to Seeders here
-//        $this->call(UserTableSeeder::class);
-//        $this->command->info('Admin User created with username admin@admin.com and password admin');
-//        $this->command->info('Test User created with username user@user.com and password user');
+        $this->call(UserTableSeeder::class);
+        $this->command->info('Admin User created with username admin@admin.com and password admin');
+        $this->command->info('Test User created with username user@user.com and password user');
 
-        $this->call(LanguageTableSeeder::class);
-
-        $this->call(ProjectsTableSeeder::class);
-        $this->command->info('Project created.');
-
-        $this->call(ProposalsTableSeeder::class);
-        $this->command->info('Proposal created.');
+        // $this->call(LanguageTableSeeder::class);
 
         Model::reguard();
     }
