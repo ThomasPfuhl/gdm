fe286a5c25dc1c106eb8031a315e1d20fc7455f5
Pfuhl, Thomas <thomas.pfuhl@mfn-berlin.de>
2017-07-03 17:31:59 +0200

    menu items cleaned up.

--- resources/views/partials/menu-items.blade.php
+++ resources/views/partials/menu-items.blade.php
@@ -1,32 +1,4 @@
 
-<li class="{{ (Request::is('agencies') ? 'active' : '') }}">
-    <a href="{{ URL::to('agencies') }}">Agencies</a>
-</li>
-
-<li class="{{ (Request::is('agents') ? 'active' : '') }}">
-    <a href="{{ URL::to('agents') }}">Agents</a>
-</li>
-
-<li class="{{ (Request::is('institutions') ? 'active' : '') }}">
-    <a href="{{ URL::to('institutions') }}">Institutions</a>
-</li>
-
-<li class="{{ (Request::is('networkPartners') ? 'active' : '') }}">
-    <a href="{{ URL::to('networkPartners') }}">NetworkPartners</a>
-</li>
-
-<li class="{{ (Request::is('networks') ? 'active' : '') }}">
-    <a href="{{ URL::to('networks') }}">Networks</a>
-</li>
-
-<li class="{{ (Request::is('projects') ? 'active' : '') }}">
-    <a href="{{ URL::to('projects') }}">Projects</a>
-</li>
-
-<li class="{{ (Request::is('proposals') ? 'active' : '') }}">
-    <a href="{{ URL::to('proposals') }}">Proposals</a>
-</li>
-
 <li class="{{ (Request::is('bars') ? 'active' : '') }}">
     <a href="{{ URL::to('bars') }}">Bars</a>
 </li>
