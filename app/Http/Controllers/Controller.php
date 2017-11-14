5284aac5038367877e2bae47087bbb91916b9478
Thomas Pfuhl <thomas.pfuhl@mfn-berlin.de>
2017-11-14 11:15:48 +0100

    bugfixes for installation procedure: Controller.php added (needed, but Swagger Info will be overwritten)

---
+++ app/Http/Controllers/Controller.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+
+/**
+  * @SWG\Swagger(  
+  *   schemes={"http","https"},  
+  *   host="localhost:8000",  
+  *   basePath="/api",  
+  *   @SWG\Info(  
+  *     version="v0_9",  
+  *     title="kitty",  
+  *     description="FB3 money pool for coffee",  
+  *     @SWG\Contact(  
+  *       email="thomas.pfuhl@mfn-berlin.de"  
+  *     ),  
+  *     @SWG\License(  
+  *       name="GNU General Public License",  
+  *       url="http://www.gnu.org/licenses/"  
+  *     )  
+  *   )
+  * )  
+  */  
+abstract class Controller extends BaseController {
+
+    use AuthorizesRequests,
+        DispatchesJobs,
+        ValidatesRequests;
+}
