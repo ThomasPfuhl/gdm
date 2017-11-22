a948dff577fd9b3a02914c34ebc35238fd95f806
Thomas Pfuhl <thomas.pfuhl@mfn-berlin.de>
2017-11-22 09:08:07 +0100

    defined current folder with pathinfo()

--- lib/tools/customize.php
+++ lib/tools/customize.php
@@ -2,15 +2,15 @@
 
 /**
  * customizing GDM   about, css, js, logos 
- * @todo find a simpler way , merge with about.blade.php
  */
 include("helpers.php");
 
 echo getcwd() . "\n";
 echo pathinfo(__FILE__)['dirname'] . "/../../.env";
 
+$cwd = pathinfo(__FILE__)['dirname'];
 
-$env = readEnvFile( pathinfo(__FILE__)['dirname'] . "/../../.env" );
+$env = readEnvFile( $cwd . "/../../.env" );
 
 $about = file_get_contents("custom/about.html");
 $about = preg_replace('/<!--(.*?)-->/Us', '', $about);
@@ -19,13 +19,13 @@ $about = str_replace('GDM_MANAGER_NAME', $env['GDM_MANAGER_NAME'], $about);
 $about = str_replace('GDM_MANAGER_EMAIL', $env['GDM_MANAGER_EMAIL'], $about);
 
 
-mkdir(getcwd() . "/public/appfiles/");
-file_put_contents(getcwd() . "/public/appfiles/about.html", $about);
+mkdir( $cwd . "/public/appfiles/");
+file_put_contents( $cwd . "/public/appfiles/about.html", $about);
 
-copy("custom/institution_logo.png", getcwd() . "/public/img/institution_logo.png");
-copy("custom/app_logo.png", getcwd() . "/public/img/app_logo.png");
+copy("custom/institution_logo.png", $cwd . "/public/img/institution_logo.png");
+copy("custom/app_logo.png", $cwd . "/public/img/app_logo.png");
 
-copy("custom/custom.css", getcwd() . "/public/css/custom.css");
-copy("custom/custom.js", getcwd() . "/public/js/custom.js");
+copy("custom/custom.css", $cwd . "/public/css/custom.css");
+copy("custom/custom.js",  $cwd . "/public/js/custom.js");
 
 echo $env['GDM_NAME'] . ": customizing done. \n";
