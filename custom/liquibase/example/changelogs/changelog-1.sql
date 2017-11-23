fb293e3767361d0f08a4851116e46996b39c5b03
Thomas Pfuhl <thomas.pfuhl@mfn-berlin.de>
2017-11-23 12:16:59 +0100

    example data module: liquibase changelogs cleanup.

--- custom/liquibase/example/changelogs/changelog-1.sql
+++ custom/liquibase/example/changelogs/changelog-1.sql
@@ -1,55 +1,12 @@
 
---
--- Table structure for table `DATABASECHANGELOG`
---
-DROP TABLE IF EXISTS `DATABASECHANGELOG`;
-CREATE TABLE `DATABASECHANGELOG` (
-  `ID` varchar(255) NOT NULL,
-  `AUTHOR` varchar(255) NOT NULL,
-  `FILENAME` varchar(255) NOT NULL,
-  `DATEEXECUTED` datetime NOT NULL,
-  `ORDEREXECUTED` int(11) NOT NULL,
-  `EXECTYPE` varchar(10) NOT NULL,
-  `MD5SUM` varchar(35) DEFAULT NULL,
-  `DESCRIPTION` varchar(255) DEFAULT NULL,
-  `COMMENTS` varchar(255) DEFAULT NULL,
-  `TAG` varchar(255) DEFAULT NULL,
-  `LIQUIBASE` varchar(20) DEFAULT NULL,
-  `CONTEXTS` varchar(255) DEFAULT NULL,
-  `LABELS` varchar(255) DEFAULT NULL,
-  `DEPLOYMENT_ID` varchar(10) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
-
---
--- Table structure for table `DATABASECHANGELOGLOCK`
---
-DROP TABLE IF EXISTS `DATABASECHANGELOGLOCK`;
-CREATE TABLE `DATABASECHANGELOGLOCK` (
-  `ID` int(11) NOT NULL,
-  `LOCKED` bit(1) NOT NULL,
-  `LOCKGRANTED` datetime DEFAULT NULL,
-  `LOCKEDBY` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`ID`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
-
---
--- Table structure for table `communities`
---
-DROP TABLE IF EXISTS `communities`;
 CREATE TABLE `communities` (
   `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
   `title` varchar(200) DEFAULT NULL COMMENT 'Title of the community',
   `description` text COMMENT 'Description of the community',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1 COMMENT='communities table';
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COMMENT='communities table';
 
 
---
--- Table structure for table `deposits`
---
-DROP TABLE IF EXISTS `deposits`;
 CREATE TABLE `deposits` (
   `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
   `amount` float unsigned DEFAULT '0',
@@ -58,39 +15,10 @@ CREATE TABLE `deposits` (
   PRIMARY KEY (`id`),
   KEY `member_id` (`member_id`),
   CONSTRAINT `deposits_ibfk_1` FOREIGN KEY (`member_id`) REFERENCES `members` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1 COMMENT='deposits table';
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COMMENT='deposits table';
 
 
---
--- Table structure for table `gdm_aggregations`
---
-DROP TABLE IF EXISTS `gdm_aggregations`;
-CREATE TABLE `gdm_aggregations` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `table_name` varchar(200) COLLATE utf8_unicode_ci NOT NULL,
-  `grouped_by_field_name` varchar(200) COLLATE utf8_unicode_ci NOT NULL,
-  `field_name` varchar(200) COLLATE utf8_unicode_ci NOT NULL,
-  `function_name` enum('AVG','COUNT','GROUP_CONCAT','MAX','MIN','SUM') COLLATE utf8_unicode_ci NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
-
---
--- Table structure for table `languages`
---
-DROP TABLE IF EXISTS `languages`;
-CREATE TABLE `languages` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `alpha2_code` char(2) COLLATE utf8_unicode_ci NOT NULL,
-  `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
-
---
--- Table structure for table `members`
---
-DROP TABLE IF EXISTS `members`;
 CREATE TABLE `members` (
   `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
   `email` varchar(200) DEFAULT NULL,
@@ -102,35 +30,10 @@ CREATE TABLE `members` (
   UNIQUE KEY `email` (`email`),
   KEY `community_id` (`community_id`),
   CONSTRAINT `members_ibfk_1` FOREIGN KEY (`community_id`) REFERENCES `communities` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=latin1 COMMENT='members table';
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COMMENT='members table';
 
 
---
--- Table structure for table `migrations`
---
-DROP TABLE IF EXISTS `migrations`;
-CREATE TABLE `migrations` (
-  `migration` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-  `batch` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
---
--- Table structure for table `password_resets`
---
-DROP TABLE IF EXISTS `password_resets`;
-CREATE TABLE `password_resets` (
-  `email` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-  `token` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-  `created_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
-  KEY `password_resets_email_index` (`email`),
-  KEY `password_resets_token_index` (`token`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
-
---
--- Table structure for table `products`
---
-DROP TABLE IF EXISTS `products`;
 CREATE TABLE `products` (
   `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
   `title` varchar(200) DEFAULT NULL,
@@ -138,29 +41,4 @@ CREATE TABLE `products` (
   `number` int(10) unsigned DEFAULT '0',
   `bought_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1 COMMENT='products table';
-
-
---
--- Table structure for table `users`
---
-DROP TABLE IF EXISTS `users`;
-CREATE TABLE `users` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-  `username` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-  `email` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-  `password` varchar(60) COLLATE utf8_unicode_ci NOT NULL,
-  `confirmation_code` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-  `confirmed` tinyint(1) NOT NULL DEFAULT '0',
-  `admin` tinyint(1) NOT NULL DEFAULT '0',
-  `language` char(2) COLLATE utf8_unicode_ci NOT NULL DEFAULT 'de',
-  `remember_token` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `created_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
-  `updated_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
-  `deleted_at` timestamp NULL DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `users_username_unique` (`username`),
-  UNIQUE KEY `users_email_unique` (`email`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COMMENT='products table';
--- custom/liquibase/example/changelogs/changelog-2.sql
+++ custom/liquibase/example/changelogs/changelog-2.sql
@@ -1,69 +1,20 @@
 
 
-
---
--- Dumping data for table `communities`
---
 LOCK TABLES `communities` WRITE;
 INSERT INTO `communities` VALUES (1,'kitty One','coffee and tea amateurs of Kitty One');
 UNLOCK TABLES;
 
-
---
--- Dumping data for table `deposits`
---
 LOCK TABLES `deposits` WRITE;
 INSERT INTO `deposits` VALUES (1,8,1,'2017-11-15 16:36:34'),(2,5,2,'2017-11-08 13:33:32'),(3,1,2,'2017-11-08 13:33:32');
 UNLOCK TABLES;
 
 
---
--- Dumping data for table `gdm_aggregations`
---
-LOCK TABLES `gdm_aggregations` WRITE;
-INSERT INTO `gdm_aggregations` VALUES (1,'deposits','member_id','amount','SUM'),(2,'products','title','number','SUM');
-UNLOCK TABLES;
-
-
---
--- Dumping data for table `languages`
---
-LOCK TABLES `languages` WRITE;
-INSERT INTO `languages` VALUES (1,'en','English');
-UNLOCK TABLES;
-
-
---
--- Dumping data for table `members`
---
 LOCK TABLES `members` WRITE;
 INSERT INTO `members` VALUES (1,'alice@example.org','Alice','A.','1234',1),(2,'bob@example.org','Bob','B.','5678',1);
 UNLOCK TABLES;
 
---
--- Dumping data for table `migrations`
---
-LOCK TABLES `migrations` WRITE;
-INSERT INTO `migrations` VALUES ('2014_10_12_000000_create_users_table',1),('2014_10_12_100000_create_password_resets_table',1),('2017_03_28_092426_create_languages_table',1),('2017_11_13_173300_create_aggregations_table',2);
-UNLOCK TABLES;
 
---
--- Dumping data for table `password_resets`
---
-LOCK TABLES `password_resets` WRITE;
-UNLOCK TABLES;
-
---
--- Dumping data for table `products`
---
 LOCK TABLES `products` WRITE;
 INSERT INTO `products` VALUES (1,'coffee','pack',1,'2017-11-15 16:40:57'),(2,'filter','pack',1,'2017-11-15 16:40:57'),(3,'milk','litre',2,'2017-11-15 16:40:57'),(4,'coffee','pack',1,'2017-11-15 16:40:57');
 UNLOCK TABLES;
 
-
---
--- Dumping data for table `users`
---
-LOCK TABLES `users` WRITE;
-INSERT INTO `users` VALUES (1,'Administrator','zeus','zeus@example.org','$2y$10$jxCrS6xpCkHZbCsLO3lTTuvushwE0bJb96jp7z7/TrU65.9TQuIBa','5c900744eab260996111e3b3d5979013',1,1,'en','NTXqfIVLXtVo2fTCLqvhXK4OSoDUbR34qWE2ZTBUWmsmPeRH0g5PaXdiHdqx','2017-10-17 09:03:32','2017-11-06 15:45:37',NULL),(2,'Default Admin','admin','admin@example.org','$2y$10$L5edhQkit77Ltg0eDmr2aekEcS1qUThpCQzI4q8qadcx79rm0WRhe','aad59a040fc48587fb9f75c10fb95ebc',1,1,'en',NULL,'2017-10-17 09:03:32','2017-10-17 09:03:32',NULL);
-UNLOCK TABLES;
